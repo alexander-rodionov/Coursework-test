eca1948bb1e3016b7c91ba9776f9b8c73a82e79b
alexander-rodionov <alexander.a.rodionov@gmail.com>
2023-11-22 23:44:46 +0300

    populate

--- test/healthcheck.py
+++ test/healthcheck.py
@@ -4,13 +4,10 @@ import contextlib
 import mariadb
 import yaml
 
-RUN_LOCAL = True
-
 with open('../docker-compose.yaml', 'r') as f:
     docker_compose = yaml.safe_load(f)
-PROXYSQL_HOST = 'proxysql' if RUN_LOCAL else '172.17.0.1'
-PROXYSQL_PORT = int('3306') if not RUN_LOCAL \
-    else int(docker_compose['services']['proxysql']['ports'][0].split(':')[0])
+PROXYSQL_HOST = 'proxysql'
+PROXYSQL_PORT = int(docker_compose['services']['proxysql']['ports'][0].split(':')[0])
 USER = 'user'
 PASSWORD = 'password'
 
